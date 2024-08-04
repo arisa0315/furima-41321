732d886d155b4ea8386354327816c08c732be8c4
arisa0315 <pyonsu0315@gmail.com>
2024-08-04 23:44:40 +0900

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end
+
   root 'items#index'
 end
