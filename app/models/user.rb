9694cf1f197fa542f137dff1913a658da943161c
arisa0315 <pyonsu0315@gmail.com>
2024-08-06 14:51:47 +0900

    修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: { message: "can't be blank" }
   validates :password, presence: { message: "can't be blank" }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
-  validates :first_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters" }
-  validates :last_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters" }
-  validates :first_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ]+\z/, message: "is invalid. Input full-width katakana characters" }
-  validates :last_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :first_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :last_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :birth_date, presence: { message: "can't be blank" }
 end
