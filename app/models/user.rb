6e47814900b310279107382409e5dd77a5583ab2
arisa0315 <pyonsu0315@gmail.com>
2024-08-09 16:07:08 +0900

    修正１０

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: { message: "can't be blank" }
   validates :password, presence: { message: "can't be blank" }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d\-]+\z/i, message: "is invalid. Include both letters, numbers, and hyphens" }
-  validates :first_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters" }
-  validates :last_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } # 修正箇所
+  validates :last_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } # 修正箇所
   validates :first_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ\-]+\z/, message: "is invalid. Input full-width katakana characters and hyphen" } # 修正箇所
   validates :last_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ\-]+\z/, message: "is invalid. Input full-width katakana characters and hyphen" } # 修正箇所
   validates :birth_date, presence: { message: "can't be blank" }
 end
-
-
