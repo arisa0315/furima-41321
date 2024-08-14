daa43ad5bae7d2286cb21007dbd135acc47c914a
arisa0315 <pyonsu0315@gmail.com>
2024-08-14 21:39:36 +0900

    イベント追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const taxDisplay = document.getElementById('add-tax-price');
   const profitDisplay = document.getElementById('profit');
 
-  if (priceInput) {
-    priceInput.addEventListener('input', () => {
-      const price = parseInt(priceInput.value, 10) || 0;
-      const tax = Math.floor(price * 0.1); // 販売手数料(10%)の計算
-      const profit = price - tax; // 販売利益の計算
+  function calculatePrice() {
+    const price = parseInt(priceInput.value, 10) || 0;
+    const tax = Math.floor(price * 0.1); // 販売手数料(10%)の計算
+    const profit = price - tax; // 販売利益の計算
 
-      taxDisplay.textContent = tax;
-      profitDisplay.textContent = profit;
-    });
+    taxDisplay.textContent = tax;
+    profitDisplay.textContent = profit;
   }
+
+  if (priceInput) {
+    priceInput.addEventListener('input', calculatePrice);
+  }
+
+  window.addEventListener("turbo:load", calculatePrice);
+  window.addEventListener("turbo:render", calculatePrice);
 });
