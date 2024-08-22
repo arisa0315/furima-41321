e85419b636e6affba2031d2adb6f6b507c32eeec
arisa0315 <pyonsu0315@gmail.com>
2024-08-22 15:24:28 +0900

    修正２

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
-  belongs_to :user
   belongs_to :item
+  belongs_to :user
+  has_one :address
+
+  validates :item_id, presence: true
+  validates :user_id, presence: true
 end
--- app/models/purchase.rb
+++
@@ -1,8 +0,0 @@
-class order < ApplicationRecord
-  belongs_to :item
-  belongs_to :user
-  has_one :address
-
-  validates :item_id, presence: true
-  validates :user_id, presence: true
-end
